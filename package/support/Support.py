36707321f275e7dcd1900f1dc5929ecd9f887b41
Main Duelo <mainduelo@gmail.com>
2018-05-22 17:16:52 -0300

    first commit

---
+++ package/support/Support.py
@@ -0,0 +1,5 @@
+class SupportInfo:
+
+    def formatText(text):
+        newText = text.replace("&", "\n")
+        return newText
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+
+
+#Basic CRUD with Tkinter windows with python 3.6.1
